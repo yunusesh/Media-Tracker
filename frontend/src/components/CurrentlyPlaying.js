205be2f98c515cb3b4651d916228b6c5afa0590b
Yunus Eshesh <yunuseshesh@gmail.com>
2025-11-13 14:35:30 -0500

    styling on player

--- frontend/src/components/CurrentlyPlaying.js
+++ frontend/src/components/CurrentlyPlaying.js
@@ -1,50 +1,91 @@
 import {AuthContext} from "../AuthContext";
-import {useContext, useEffect, useState} from "react";
+import {useContext, useEffect, useRef, useState} from "react";
 import {SpotifyAuthContext} from "../SpotifyAuthContext";
 import "./CurrentlyPlaying.css"
+import {TiPinOutline} from "react-icons/ti";
 
 export function CurrentlyPlaying() {
     const {user} = useContext(AuthContext)
     const {fetchCurrentlyPlaying, spotifyToken} = useContext(SpotifyAuthContext)
     const [currentlyPlaying, setCurrentlyPlaying] = useState(null)
+    const [pinned, setPinned] = useState(false)
+    const containerRef = useRef(null);
+    const textRef = useRef(null);
+    const [scrollDistance, setScrollDistance] = useState(0);
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            if (spotifyToken) {
-                async function fetchCurrent() {
-                    try {
-                        const current = await fetchCurrentlyPlaying();
-                        setCurrentlyPlaying(current.item)
-                    } catch (error) {
-                        console.log(error, "No track playing")
-                    }
-                }
+        if (!spotifyToken) return;
+        let interval = setInterval(() => {
+            async function fetchCurrent() {
+                try {
+                    const response = await fetchCurrentlyPlaying();
+                    setCurrentlyPlaying(response.item)
 
-                fetchCurrent()
+                    if (containerRef.current && textRef.current) {
+                        const containerWidth = containerRef.current.offsetWidth;
+                        const textWidth = textRef.current.scrollWidth;
+
+                        if (textWidth > containerWidth) {
+                            console.log(textWidth, containerWidth, textWidth - containerWidth);
+                            setScrollDistance(textWidth - containerWidth);
+                        } else {
+                            setScrollDistance(0);
+                        }
+                    }
+                } catch (error) {
+                    if (error.status === 429) {
+                        console.warn("Rate limit exceeded -- wait 10 seconds")
+                        clearInterval(interval)
+                        interval = setInterval(fetchCurrent, 10000)
+                    } else console.log(error, "No track playing")
+                }
             }
+
+            fetchCurrent()
+
         }, 3000)
+
+        return () => clearInterval(interval);
     })
 
     return (
-        <div className="currently-playing-container">
+        <div className={pinned ? "currently-playing-container-pinned" : "currently-playing-container"}>
             {spotifyToken && currentlyPlaying && (
                 <div className="player">
-                    <img className = "player-img"
-                         src = {currentlyPlaying.album.images[2].url}
-                         alt = "placeholder.png"/>
-
-                    <div className="player-info">
-                        <h3 className="player-track">{currentlyPlaying.name}</h3>
-                        <div className="player-artist">
-                            {currentlyPlaying.artists.map((artist, index, array) => (
-                                <span key={artist.id}>
+                    <TiPinOutline className={pinned ? "pin-active" : "pin"}
+                                  onClick={() => {
+                                      setPinned(!pinned)
+                                  }}/>
+                    <div className="player-left">
+                        <img className="player-img"
+                             src={currentlyPlaying.album.images[2].url}
+                             alt="placeholder.png"/>
+                        <div className="player-info" ref = {containerRef}>
+                            <h3 className="player-track"
+                                ref = {textRef}
+                                style={{
+                                    animation: scrollDistance
+                                        ? `scrollText ${scrollDistance/30 + 3}s linear infinite alternate`
+                                        : "none",
+                                }}
+                            >{currentlyPlaying.name}</h3>
+                            <div className="player-artist">
+                                {currentlyPlaying.artists.map((artist, index, array) => (
+                                    <span key={artist.id}>
                                     <h3>{artist.name}</h3>
-                                    {index < array.length - 1 && (
-                                        <span>{index === array.length - 2 ? " & " : ", "}</span>
-                                    )}
+                                        {index < array.length - 1 && (
+                                            <span>{index === array.length - 2 ? " & " : ", "}</span>
+                                        )}
                                 </span>
-                            ))}
+                                ))}
+                            </div>
                         </div>
+                    </div>
+                    <div className="equalizer-container">
+                        <div className="bar-1"></div>
+                        <div className="bar-2"></div>
+                        <div className="bar-3"></div>
+
                     </div>
                 </div>
             )}
